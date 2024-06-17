570c86e1adf8c7d583b3d6f108098e8849bba478
Subrat <subratdash2022@gmail.com>
2024-06-17 08:42:06 +0530

    Initial commit

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# User Credentials
+host = "localhost"
+user = "root"
+password = "my@67_sqlP"
+database = "library_management"
---
+++ customs.py
@@ -0,0 +1,25 @@
+color_1 = "deep sky blue"
+color_2 = "gray95"
+color_3 = "black"
+color_4 = "white"
+color_5 = "red"
+color_6 = "green3"
+color_7 = "yellow"
+color_8 = "blue"
+color_9 = "pink"
+color_10 = "purple"
+font_1 = "times new roman"
+font_2 = "helvetica"
+
+columns = ("book_id", "book_name", "author", "edition", "price", "qty")
+
+columns_1 = (
+    "book_id",
+    "book_name",
+    "student_roll",
+    "student_name",
+    "course",
+    "subject",
+    "issue_date",
+    "return_date",
+)
